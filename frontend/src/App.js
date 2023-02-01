735069bc9fb2d7bd9e972e68804abeac24879683
Derick Hooley <dghooley@gmail.com>
2023-02-01 08:47:10 -0600

    Routed pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
+import LoginPage from "./pages/LoginPage";
+import ProductDetailsPage from "./pages/ProductDetails";
+import ProductListPage from "./pages/ProductListPage";
+import RegisterPage from "./pages/RegisterPage";
 
 
 function App() {
@@ -8,6 +13,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/home" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/product-details" element={<ProductDetailsPage />} />
+        <Route path="/product-list" element={<ProductListPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
 
