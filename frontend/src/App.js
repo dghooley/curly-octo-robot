e9fbedaedf7a274cf24ca24b920f61e91d2592ea
Derick Hooley <dghooley@gmail.com>
2023-02-04 20:20:29 -0600

    Created Routes for Protected Pages for Admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,35 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 import LoginPage from "./pages/LoginPage";
+import ProtectedRoutesComponent from "./components/ProtectedRoutesComponent";
 import ProductDetailsPage from "./pages/ProductDetails";
 import ProductListPage from "./pages/ProductListPage";
 import RegisterPage from "./pages/RegisterPage";
+
+// protected user pages: 
 import UserProfilePage from "./pages/user/UserProfilePage";
 import UserOrdersPage from "./pages/user/UserOrdersPage";
 import UserCartDetailsPage from "./pages/user/UserCartDetailsPage";
 import UserOrderDetailsPage from "./pages/user/UserOrderDetailsPage";
-import ProtectedRoutesComponent from "./components/ProtectedRoutesComponent";
+
+// protected admin pages
+import AdminUsersPage from "./pages/admin/AdminUsersPage";
+import AdminEditUserPage from "./pages/admin/AdminEditUserPage";
+import AdminProductsPage from "./pages/admin/AdminProductsPage";
+import AdminCreateProductPage from "./pages/admin/AdminCreateProductPage";
+import AdminEditProductPage from "./pages/admin/AdminEditProductPage";
+import AdminOrdersPage from "./pages/admin/AdminOrdersPage";
+import AdminOrderDetailsPage from "./pages/admin/AdminOrderDetailsPage";
+import AdminChatsPage from "./pages/admin/AdminChatsPage";
+import AdminAnalyticsPage from "./pages/admin/AdminAnalyticsPage";
 
 function App() {
   return (
 
     <BrowserRouter>
       <Routes>
+
+
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/login" element={<LoginPage />} />
@@ -27,12 +42,25 @@ function App() {
 
         {/*  <Route path="/" component{HomePage} /> in previous versions of react-router-dom */}
 
-        <Route element-={< ProtectedRoutesComponent />}>
-
+        {/* user protected routes: */}
+        <Route element-={< ProtectedRoutesComponent admin={false} />}>
           <Route path="/user" element={<UserProfilePage />} />
           <Route path="/user/my-orders" element={<UserOrdersPage />} />
           <Route path="/user/cart-details" element={<UserCartDetailsPage />} />
           <Route path="/user/order-details" element={<UserOrderDetailsPage />} />
+        </Route>
+
+        {/* admin protected routes: */}
+        <Route element-={< ProtectedRoutesComponent admin={true} />}>
+          <Route path="/admin/users" element={<AdminUsersPage />} />
+          <Route path="/admin/edit-user" element={<AdminEditUserPage />} />
+          <Route path="/admin/products" element={<AdminProductsPage />} />
+          <Route path="/admin/create-new-product" element={<AdminCreateProductPage />} />
+          <Route path="/admin/edit-product" element={<AdminEditProductPage />} />
+          <Route path="/admin/orders" element={<AdminOrdersPage />} />
+          <Route path="/admin/order-details" element={<AdminOrderDetailsPage />} />
+          <Route path="/admin/chats" element={<AdminChatsPage />} />
+          <Route path="/admin/analytics" element={<AdminAnalyticsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/admin/AdminOrderDetailsPage.js
@@ -0,0 +1,6 @@
+const AdminOrderDetailsPage = () => {
+
+    return <p>This is an order details page</p>
+}
+
+export default AdminOrderDetailsPage;
